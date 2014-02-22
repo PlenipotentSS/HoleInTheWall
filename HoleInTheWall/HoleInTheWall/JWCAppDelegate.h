4d907f87b0c55dd6a95166d8d149c375e59e8436
Jeff Schwab <jschwab923@gmail.com>
2014-02-21 16:00:15 -0800

    Initial Commit

---
+++ HoleInTheWall/HoleInTheWall/JWCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JWCAppDelegate.h
+//  HoleInTheWall
+//
+//  Created by Jeff Schwab on 2/21/14.
+//  Copyright (c) 2014 Jeff Schwab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JWCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HoleInTheWall/HoleInTheWall/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HoleInTheWall
+//
+//  Created by Jeff Schwab on 2/21/14.
+//  Copyright (c) 2014 Jeff Schwab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JWCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JWCAppDelegate class]));
+    }
+}
