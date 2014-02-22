b882274cb62206992899e1088418a9c41d016fd8
PlenipotentSS <stevensonhoyt@icloud.com>
2014-02-21 16:12:43 -0800

    added game scene

---
+++ HoleInTheWall/HoleInTheWall/SS/SSGameScene.h
@@ -0,0 +1,13 @@
+//
+//  SSGameScene.h
+//  HoleInTheWall
+//
+//  Created by Stevenson on 21/02/2014.
+//  Copyright (c) 2014 Jeff Schwab. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface SSGameScene : SKScene
+
+@end
