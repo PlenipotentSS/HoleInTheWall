117fc718b66652e01c52b0366c877ae4d47b3d38
Matt Remick <mremick@gmail.com>
2014-02-25 19:13:18 -0800

    added opening scene, changed root view controller

---
+++ HoleInTheWall/HoleInTheWall/JWCOpeningSceneViewController.h
@@ -0,0 +1,15 @@
+//
+//  JWCOpeningSceneViewController.h
+//  HoleInTheWall
+//
+//  Created by Matt Remick on 2/25/14.
+//  Copyright (c) 2014 Jeff Schwab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+
+@interface JWCOpeningSceneViewController : UIViewController
+
+@end
---
+++ HoleInTheWall/HoleInTheWall/JWCOpeningSceneViewController.m
@@ -0,0 +1,52 @@
+//
+//  JWCOpeningSceneViewController.m
+//  HoleInTheWall
+//
+//  Created by Matt Remick on 2/25/14.
+//  Copyright (c) 2014 Jeff Schwab. All rights reserved.
+//
+
+#import "JWCOpeningSceneViewController.h"
+#import "OpeningScene.h"
+
+@interface JWCOpeningSceneViewController ()
+
+@end
+
+@implementation JWCOpeningSceneViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    self.view = [SKView new];
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewWillLayoutSubviews
+{
+    // Testing wall
+    SKView *gameView = (SKView *)self.view;
+    
+    OpeningScene *scene = [OpeningScene sceneWithSize:gameView.frame.size];
+    scene.scaleMode = SKSceneScaleModeFill;
+    
+    [gameView presentScene:scene];
+}
+
+@end
