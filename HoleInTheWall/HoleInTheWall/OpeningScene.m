6ecfa87170a67eb1cf65730662adae012cdae030
Matt Remick <mremick@gmail.com>
2014-02-25 20:47:57 -0800

    added skactions to the opening scence

--- HoleInTheWall/HoleInTheWall/OpeningScene.m
+++ HoleInTheWall/HoleInTheWall/OpeningScene.m
@@ -8,44 +8,52 @@
 
 #import "OpeningScene.h"
 
+@interface OpeningScene()
+
+@property (strong,nonatomic) SKLabelNode *holeLabel;
+@property (strong,nonatomic) SKLabelNode *inTheLabel;
+@property (strong,nonatomic) SKLabelNode *wallLabel;
+
+@end
+
 @implementation OpeningScene
 
 -(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
+        
 
-        SKLabelNode* holeLabel = [SKLabelNode labelNodeWithFontNamed:@"Prisma"];
-        holeLabel.fontSize = 50;
-        holeLabel.fontColor = [SKColor yellowColor];
-        holeLabel.zPosition = 100;
+        self.holeLabel = [SKLabelNode labelNodeWithFontNamed:@"Prisma"];
+        self.holeLabel.fontSize = 50;
+        self.holeLabel.fontColor = [SKColor yellowColor];
+        self.holeLabel.zPosition = 100;
         
-        holeLabel.text = @"Hole";
-        holeLabel.position = CGPointMake(self.size.width / 2, self.size.height - 180);
-        [self addChild:holeLabel];
+        self.holeLabel.text = @"Hole";
+        self.holeLabel.position = CGPointMake(-600, self.size.height - 180);
+        [self addChild:self.holeLabel];
         
-        SKLabelNode* inTheLabel = [SKLabelNode labelNodeWithFontNamed:@"Prisma"];
-        inTheLabel.fontSize = 50;
-        inTheLabel.fontColor = [SKColor yellowColor];
-        inTheLabel.zPosition = 100;
+        self.inTheLabel = [SKLabelNode labelNodeWithFontNamed:@"Prisma"];
+        self.inTheLabel.fontSize = 50;
+        self.inTheLabel.fontColor = [SKColor yellowColor];
+        self.inTheLabel.zPosition = 100;
         
-        inTheLabel.text = @"In The";
-        inTheLabel.position = CGPointMake(self.size.width / 2, self.size.height - 225);
-        [self addChild:inTheLabel];
+        self.inTheLabel.text = @"In The";
+        self.inTheLabel.position = CGPointMake(600, self.size.height - 250);
+        [self addChild:self.inTheLabel];
         
-        SKLabelNode* wallLabel = [SKLabelNode labelNodeWithFontNamed:@"Prisma"];
-        wallLabel.fontSize = 100;
-        wallLabel.fontColor = [SKColor yellowColor];
-        wallLabel.zPosition = 100;
+        self.wallLabel = [SKLabelNode labelNodeWithFontNamed:@"Prisma"];
         
-        wallLabel.text = @"WALL";
-        wallLabel.position = CGPointMake(self.size.width / 2, self.size.height - 320);
-        [self addChild:wallLabel];
+        self.wallLabel.hidden = YES;
         
-        SKAction *moveForwardAction = [SKAction scaleTo:1 duration:0.5];
-        SKAction *scaleOffAction = [SKAction scaleTo:175 duration:0.5];
+        self.wallLabel.fontSize = 100;
+        self.wallLabel.fontColor = [SKColor yellowColor];
+        self.wallLabel.zPosition = 100;
         
-        SKAction *scaleBack = [SKAction scaleTo:1 duration:1.5];
+        self.wallLabel.text = @"WALL";
+        self.wallLabel.position = CGPointMake(self.size.width / 2, self.size.height - 320);
+        [self addChild:self.wallLabel];
         
+        [self performSelector:@selector(performSKActions) withObject:nil afterDelay:.4];
         
 //        [wallLabel runAction:moveForwardAction completion:^{
 //            [wallLabel runAction:scaleOffAction completion:^{
@@ -53,26 +61,43 @@
 //            }];
 //        }];
         
-        [holeLabel runAction:moveForwardAction completion:^{
-            [holeLabel runAction:scaleOffAction completion:^{
-                [inTheLabel runAction:moveForwardAction completion:^{
-                    [inTheLabel runAction:scaleOffAction completion:^{
-                        [wallLabel runAction:moveForwardAction completion:^{
-                            [wallLabel runAction:scaleOffAction completion:^{
-                                [holeLabel runAction:scaleBack];
-                                [inTheLabel runAction:scaleBack];
-                                [wallLabel runAction:scaleBack];
-                                
-                                
-                            }];
-                        }];
-
-                    }];
-                }];
-            }];
-        }];
+        
     }
     return self;
+}
+
+- (void)performSKActions
+{
+    SKAction *moveForwardAction = [SKAction scaleTo:1 duration:0.5];
+    SKAction *scaleOffAction = [SKAction scaleTo:100 duration:2.0];
+    
+    SKAction *moveInHoleLabel = [SKAction moveTo:CGPointMake(self.size.width / 2, self.size.height - 180) duration:.4];
+    SKAction *moveInInTheLabel = [SKAction moveTo:CGPointMake(self.size.width / 2, self.size.height - 225) duration:.4];
+    
+    [self.holeLabel runAction:moveInHoleLabel completion:^{
+        [self.inTheLabel runAction:moveInInTheLabel completion:^{
+            self.wallLabel.hidden = NO;
+            [self performSelector:@selector(scaleWall) withObject:nil afterDelay:.5];
+            
+        }];
+    }];
+    
+}
+
+- (void)scaleWall
+{
+    SKAction *scaleWall = [SKAction scaleTo:23 duration:2.4];
+    [self.wallLabel runAction:scaleWall completion:^{
+        self.wallLabel.hidden = YES;
+        SKAction *scaleWallToNormal = [SKAction scaleTo:1 duration:0.01];
+        [self.wallLabel runAction:scaleWallToNormal completion:^{
+            self.wallLabel.alpha = 0;
+            self.wallLabel.hidden = NO;
+            SKAction *fadeWallLabelIn = [SKAction fadeInWithDuration:2.0];
+            [self.wallLabel runAction:fadeWallLabelIn];
+            
+        }];
+    }];
 }
 
 @end
